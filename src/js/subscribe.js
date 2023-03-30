d33a9d5f6686142a05e175dc1354b3aa80fc45a1
AntoninaGraur <124353835+AntoninaGraur@users.noreply.github.com>
2023-03-30 19:27:50 +0300

    subscrb_modal_form

---
+++ src/js/subscribe.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-sbscrb_modal-open]"),
+    closeModalBtn: document.querySelector("[data-sbscrb_modalclose]"),
+    sbscrb_modal: document.querySelector("[sbscrb_modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.sbscrb_modal.classList.toggle("is-open");
+  }
+})();
