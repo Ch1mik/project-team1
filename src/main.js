c9935e61a0c83214328330001ce6c6f8d077a8c7
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-03-31 14:58:18 +0300

    hghg
    hghg

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './js/mobile-menu';
 import './js/our-products';
 import './js/reviews';
@@ -5,3 +6,10 @@ import './js/subscribe';
 import './js/buy-now';
 import './js/popup';
 import "./js/rewiews-modal"
+=======
+import"./js/mobile-menu"
+import"./js/our-products"
+import"./js/reviews"
+import "./js/subscribe"
+import"./js/popup"
+>>>>>>> Stashed changes
